5e37a560fb5b9ffacde3d44bbb98475a5b2c8663
Elowynne Xiong <chackyxyooj@hotmail.com>
2026-02-19 21:01:41 -0500

    Tests Added to Services
    These tests will be run VIA the pipeline & will leave artifacts after being run.

--- ProductService/src/test/java/org/example/productservice/ProductServiceTest.java
+++ ProductService/src/test/java/org/example/productservice/ProductServiceTest.java
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class ProductControllerTest {
+class ProductServiceTest {
 
     @Mock
     private ProductRepository productRepository;
